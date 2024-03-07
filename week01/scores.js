8ab97dfdd3d5eb294b41abad2e731c84b249a64a
Fulvio Corno <fulvio.corno@polito.it>
2024-03-07 10:08:24 +0100

    L03 exercises

--- week01/scores.js
+++ week01/scores.js
@@ -1,3 +1,63 @@
 // Exercise 1: Better Scores
 "use strict";
 
+const scores = [5, 8, -2, -9, 4, 5, -2, 9] ;
+// let  scores = [5, 8, -2, -9, 4, 5, -2, 10] ;
+scores[0] = 6 ;
+scores.push(-9)
+// scores = [...scores, 3] ;
+
+console.log(scores);
+console.log(scores.length);
+
+for (const score of scores) {
+    console.log(`Your score is ${score}`);
+}
+
+// const scores2 = scores ; NO, just a reference, not a copy
+const scores3 = [...scores];
+
+// for(let i = 0; i < scores.length; i++) {
+//     if(scores3[i]<0) {
+//         scores3.splice(i, 1) ;
+//         i--;
+//     }
+// }
+
+const positiveScores = [];
+for(const score of scores3) {
+    if(score>=0) {
+        positiveScores.push(score);
+    }
+}
+const NN = scores3.length - positiveScores.length;
+console.log(positiveScores);
+
+// NO becaue sort implmements a string ordering
+// positiveScores.sort();
+// console.log(positiveScores);
+
+// console.log(scores3);
+
+for(let repeat = 0; repeat<2; repeat++) {
+    let posmin = 0 ;
+    for(let i=0; i<positiveScores.length; i++) {
+        if(positiveScores[i]<positiveScores[posmin]) {
+            posmin = i;
+        }
+    }
+    positiveScores.splice(posmin, 1);    
+}
+console.log(positiveScores);
+
+let avg = 0;
+for(const score of positiveScores) {
+    avg += score;
+}
+avg = Math.round(avg / positiveScores.length);
+
+for (let count=0; count<NN+2; count++) {
+    positiveScores.push(avg)
+}
+
+console.log(positiveScores);
--- week01/users.js
+++ week01/users.js
@@ -1,3 +1,27 @@
 // Exercise 2: My Users' List
 "use strict";
 
+const users = "Luigi De   Russis,Luca Mannella, Fulvio Corno,  Juan      Pablo Saenz Moreno , Luca Pezzolla"
+const userArray = users.split(",");
+for(let i =0; i<userArray.length; i++) {
+    userArray[i] = userArray[i].trim();
+}
+console.log(userArray);
+
+const acronyms = [] ;
+for (const name of userArray) {
+
+    // alternative: consider using 'split': console.log(name.split(' '));
+
+    let acronym = name[0];
+
+    for(let i = 1; i<name.length; i++) {
+         if(name[i] == ' ' && name[i+1] != ' ') {
+            acronym = acronym + name[i+1]; 
+         }
+    }
+
+    acronyms.push(acronym.toUpperCase())
+}
+
+console.log(acronyms)
