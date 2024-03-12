b9133b3a902e7721c18a8fb1cf11485652dc340d
Fulvio Corno <fulvio.corno@polito.it>
2024-03-12 22:59:03 +0100

    exercise completed, test code added

--- week02/ex3.mjs
+++ week02/ex3.mjs
@@ -18,20 +18,41 @@ function Question(text, user, date) {
     this.answers = [] ;
 
     this.add = (answer) => { this.answers.push(answer) }
+    this.find = (user) => this.answers.filter(a => a.user === user) ;
+    this.afterDate = (date) => this.answers.filter(a => (a.date.isAfter(date) || a.date.isSame(date)))
+    this.listByDate = () => {
+        const ans = [...this.answers] ;
+        ans.sort((a1, a2)=>(a1.date.valueOf()-a2.date.valueOf())) ;
+        ans.sort((a1, a2)=> {
+            if(a1.date.isBefore(a2.date))
+                return -1 ;
+            else if(a1.date.isAfter(a2.date))
+                return +1 ;
+            else 
+                return 0 ;
+        })
+        return ans
+    }
+    this.listByScore = () => {
+        const ans = [...this.answers]
+        ans.sort((a1, a2)=>a1.score-a2.score)
+        return ans
+    }
 }
 
-const d = dayjs()
-console.log(d)
-const a1 = new Answer('Yes', 'fulvio', 5, '2024-03-12')
-console.log(a1)
-a1.voteUp()
-console.log(a1)
-console.log(a1.getCategory())
+const q1 = new Question('Are you happy?', 'fulvio', dayjs('2024-03-12')) ;
 
-const q1 = new Question('Are you happy?', 'fulvio', '2024-03-12') ;
-    console.log(typeof q1) // 'object'
-    console.log(q1.constructor) // the Question object reference
-    console.log(q1 instanceof Question) // true
-console.log(q1)
+const a1 = new Answer('Yes', 'fulvio', 5, dayjs('2024-03-12'))
 q1.add(a1)
-console.log(q1)
+
+q1.add(new Answer('Maybe', 'luigi', 0, dayjs()))
+q1.add(new Answer('No', 'fulvio', -2, dayjs('2024-03-12')))
+q1.add(new Answer('Undecided', 'luigi', 0, dayjs()))
+
+// console.log(q1.find('luigi'))
+
+// console.log(q1.afterDate(dayjs('2024-03-12')))
+
+// console.log(q1.listByDate())
+
+// console.log(q1.listByScore())
